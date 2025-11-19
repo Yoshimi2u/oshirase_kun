0f30c84fb888e53fd1cbd212f21d01f4ccc825d5
Yoshimizu <matuno0714@outlook.jp>
2025-11-19 17:53:08 +0900

    ビルド番号更新

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 1
+        versionCode = 4
         versionName = "1.0.0"
     }
 
